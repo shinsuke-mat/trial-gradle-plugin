6cca4c1ee2f94e073b78f4c283e018f9e8bfe71a
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2019-07-12 15:46:57 +0900

    reformat

--- src/main/groovy/sample/gradle/plugin/MyGradlePlugin.groovy
+++ src/main/groovy/sample/gradle/plugin/MyGradlePlugin.groovy
@@ -5,12 +5,12 @@ import org.gradle.api.Project
 
 class MyGradlePlugin implements Plugin<Project> {
 
-    @Override
-    void apply(Project project) {
-        project.task('hello') {
-        
-            project.extensions.create('hello', MyGradlePluginConfiguration);
-            println "Hello My Gradle Plugin!! value = ${project.hello.value}"
-        }
+  @Override
+  void apply(Project project) {
+    project.task('hello') {
+
+      project.extensions.create('hello', MyGradlePluginConfiguration);
+      println "Hello My Gradle Plugin!! value = ${project.hello.value}"
     }
+  }
 }
