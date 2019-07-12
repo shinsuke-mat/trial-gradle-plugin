e01dced99619c1b0f875956f0597607b464cf5be
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2019-07-12 14:31:07 +0900

    init

---
+++ src/main/groovy/sample/gradle/plugin/MyGradlePlugin.groovy
@@ -0,0 +1,14 @@
+package sample.gradle.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MyGradlePlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.task('hello') {
+            println 'Hello My Gradle Plugin!!'
+        }
+    }
+}
